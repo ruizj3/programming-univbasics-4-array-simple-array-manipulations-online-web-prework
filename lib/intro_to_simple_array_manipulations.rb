ab79b5fd6b7111b1b3ff2eeba71a960aadbe225c
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:38:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,7 +46,6 @@ def using_flatten(instruments)
 end
 
 def using_delete(instructors)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
 end
 
