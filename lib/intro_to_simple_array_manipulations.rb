cb93a75611ba37973f66cf5d5f78201cfda73a04
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:30:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,7 @@ end
 def using_insert(list_of_programming_languages,another_language)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
-  list_of_programming_languages[4].insert(another_language)
+  list_of_programming_languages[3].insert(another_language)
 end
 
 def using_uniq(haircuts)
