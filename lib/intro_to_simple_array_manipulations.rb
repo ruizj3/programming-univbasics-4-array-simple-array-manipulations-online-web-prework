66868656131a14d3bd64a8f8f398d614caf1a21f
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:37:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,8 +28,6 @@ def shift_with_args(ice_cream_brands)
 end
 
 def using_concat(my_favorite_things,more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
   my_favorite_things.concat(more_favs)
 end
 
