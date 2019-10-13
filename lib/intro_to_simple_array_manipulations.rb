f91f503cddccd57f89fb4662186bb62c512e007c
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:35:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 def using_pop(continents)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents = []
   continents.pop
 end
 
