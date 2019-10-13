c220f966a67a708588f4b8384522ab2e584fe6b4
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:01:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,7 +57,7 @@ def using_delete(instructors)
   no_offense_steven = instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots,2)
+def using_delete_at(famous_robots)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
   deleted_robot = famous_robots.delete_at(2)
 end
