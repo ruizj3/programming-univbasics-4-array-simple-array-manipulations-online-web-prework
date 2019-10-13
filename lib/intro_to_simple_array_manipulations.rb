7bdbbcfa89e989d62c2efb389c6ab7098780f3a5
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:37:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,8 +32,6 @@ def using_concat(my_favorite_things,more_favs)
 end
 
 def using_insert(list_of_programming_languages,another_language)
-  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  another_language = "Python"
   list_of_programming_languages[3].insert(another_language)
 end
 
