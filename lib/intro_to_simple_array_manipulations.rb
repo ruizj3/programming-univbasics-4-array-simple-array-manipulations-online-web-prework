ea2f09504e4e0b2d6b55707d95c1a777cdb4475b
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 21:37:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,7 +19,6 @@ def pop_with_args(dog_breeds)
 end
 
 def using_shift(my_favorite_cities)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   my_favorite_cities.shift
 end
 
